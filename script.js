1b866a44c3e25a7b9c2da22cf3a7363a27d30dfb
Chilli01 <charlie6.evans@live.uwe.ac.uk>
2020-10-11 22:51:03 +0100

    added a fixed header, updated homepage, added skill page

---
+++ script.js
@@ -0,0 +1,16 @@
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("headerID");
+var container = header.offsetTop;
+
+function myFunction() 
+{
+  if (window.pageYOffset > container) 
+  {
+    header.classList.add("headerToggle");
+  } 
+  else 
+  {
+    header.classList.remove("headerToggle");
+  }
+}
