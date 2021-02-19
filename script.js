e0f3b0f4810ed821010ae81565609d284e68fb94
Charlie <charlie6.evans@live.uwe.ac.uk>
2021-02-19 21:17:15 +0000

    added blog and contact section

--- script.js
+++ script.js
@@ -1,32 +1,12 @@
-// Fixed Nav bar
 window.onscroll = function() {myFunction()};
-var header = document.getElementById("headerID");
-var container = header.offsetTop;
 
-function myFunction() 
-{
-  if (window.pageYOffset > container) 
-  {
-    header.classList.add("headerToggle");
-  } 
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
 
-  else 
-  {
-    header.classList.remove("headerToggle");
-  }
-}
-
-// Menu Dropdown
-function dropdownMenu() 
-{
-  var nav_bar = document.getElementById("myLinks");
-  if (nav_bar.style.display === "block") 
-  {
-    nav_bar.style.display = "none";
-  } 
-
-  else 
-  {
-    nav_bar.style.display = "block";
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
   }
 }
